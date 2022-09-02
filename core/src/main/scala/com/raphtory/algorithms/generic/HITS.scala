f37d3f09bfb28c92d5c8459a9b053c52dbbf7ee5
Natalie Shapiro <natalie.shapiro@pometry.com>
2022-09-02 17:38:36 +0100

    Added normalisation, treat auth/hub messages differently to send to in/out neighbours, results not quite right

--- core/src/main/scala/com/raphtory/algorithms/generic/HITS.scala
+++ core/src/main/scala/com/raphtory/algorithms/generic/HITS.scala
@@ -1,45 +1,102 @@
 package com.raphtory.algorithms.generic
+
 import com.raphtory.algorithms.generic.NodeList
 import com.raphtory.api.analysis.graphview.GraphPerspective
 
 class HITS(iterateSteps: Int = 100) extends NodeList(Seq("hitshub", "hitsauth")) {
 
-  override def apply(graph: GraphPerspective): graph.Graph =
+  // normalising on previous max isn't quite right
+  override def apply(graph: GraphPerspective): graph.Graph = {
+    val initHub = 1.0;
     graph
-      .step { vertex =>
-        println("hi")
-        val initHub = 1.0; val initAuth = 1.0;
+      .setGlobalState { state =>
+        state.newMax[Double]("hubAuthMax", retainState = false)
+      }
+      .step { (vertex, state) =>
         vertex.setState("hitshub", initHub)
-        vertex.setState("hitsauth", initAuth)
+
         val outDegree = vertex.outDegree
         if (outDegree > 0.0)
-          vertex.messageOutNeighbours(List(initHub / outDegree, initAuth / outDegree)) // seq?
+          vertex.messageOutNeighbours(initHub)
+      }
+      .step { (vertex, state) =>
+        val firstHub = vertex.getState[Double]("hitshub") // fix so not double, msg types
+
+        val queue     = vertex.messageQueue[Double]
+        val firstAuth = queue.sum / initHub //todo figure out normalising
+        vertex.setState("hitsauth", firstAuth)
+
+        state("hubAuthMax") += initHub
+        state("hubAuthMax") += firstAuth
+
+        val outDegree = vertex.outDegree
+        val inDegree  = vertex.inDegree
+
+        if (outDegree > 0.0)
+          vertex.messageOutNeighbours(HubMsg(firstHub)) // seq?
+        if (inDegree > 0.0)
+          vertex.messageInNeighbours(AuthMsg(firstAuth))
       }
       .iterate(
-        { vertex =>
-          val currHub = vertex.getState[Double]("hitshub")
-          val currAuth = vertex.getState[Double]("hitsauth")
-          val queue = vertex.messageQueue[List[Double]]
+              { (vertex, state) =>
+                val hubAuthMax: Double = state("hubAuthMax").value
 
-          val newHub = queue.map(l => l(0)).sum
-          val newAuth = queue.map(l => l(1)).sum
+                val currHub  = vertex.getState[Double]("hitshub")
+                val currAuth = vertex.getState[Double]("hitsauth")
+                val queue    = vertex.messageQueue[Message]
 
-          println(newHub, newAuth)
+                var newAuth: Double = 0.0
+                var newHub: Double  = 0.0
 
-          vertex.setState("hitshub", newHub)
-          vertex.setState("hitsauth", newAuth)
+                queue.foreach {
+                  case HubMsg(value)  =>
+                    newHub += (value / hubAuthMax) // normalise
 
-          val outDegree = vertex.outDegree
+                  case AuthMsg(value) =>
+                    newAuth += (value / hubAuthMax)
+                }
+                //val newAuth = queue.map(l => l(0)).sum / authMax.max(hubMax)
+                //val newHub = queue.map(l => l(1)).sum / authMax.max(hubMax)
 
-          if (outDegree > 0)
-            vertex.messageOutNeighbours(List(newHub / outDegree, newAuth / outDegree))
+                state("hubAuthMax") += newHub
+                state("hubAuthMax") += newAuth
+                println(newHub, newAuth)
 
-          if (Math.abs(currHub - newHub) < 0.01 & Math.abs(currAuth - newAuth) < 0.01)
-            vertex.voteToHalt()
-        },
-        iterations = iterateSteps,
-        executeMessagedOnly = false
+                vertex.setState("hitshub", newHub)
+                vertex.setState("hitsauth", newAuth)
+
+                val outDegree = vertex.outDegree
+                val inDegree  = vertex.inDegree
+
+                if (outDegree > 0.0)
+                  vertex.messageOutNeighbours(HubMsg(newHub)) // seq?
+                if (inDegree > 0.0)
+                  vertex.messageInNeighbours(AuthMsg(newAuth))
+
+                if (Math.abs(currHub - newHub) < 0.00001 & Math.abs(currAuth - newAuth) < 0.00001)
+                  vertex.voteToHalt()
+              },
+              iterations = iterateSteps,
+              executeMessagedOnly = false
       )
+      .step { (vertex, state) =>
+        // normalise the final values
+        val hubAuthMax: Double = state("hubAuthMax").value
+
+        val currHub  = vertex.getState[Double]("hitshub")
+        val currAuth = vertex.getState[Double]("hitsauth")
+
+        val newAuth = currAuth / hubAuthMax
+        val newHub  = currHub / hubAuthMax
+
+        vertex.setState("hitshub", newHub)
+        vertex.setState("hitsauth", newAuth)
+      }
+  }
+
+  sealed trait Message {}
+  case class HubMsg(value: Double)  extends Message
+  case class AuthMsg(value: Double) extends Message
 }
 
 object HITS {
